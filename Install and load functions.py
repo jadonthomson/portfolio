c1d0e22e70dc1e9adf1a79b884c720a81c917008
Jadon Thomson <jadon.thomson@gmail.com>
2021-02-09 21:37:06 +0000

    initializing project

---
+++ Install and load functions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 19:36:59 2021
+
+@author: jadon
+"""
+
+print("Checking that required packages are installed and loading functions")
